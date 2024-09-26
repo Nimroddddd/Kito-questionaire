dba0b48ebf45d07ec46c9e9085cec9e9fd6bac2b
auf <azeezumarfaruk@gmail.com>
2024-09-26 15:44:15 +0100

    commented out the modifying password in models

--- src/models/User.js
+++ src/models/User.js
@@ -13,15 +13,15 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
+// UserSchema.pre("save", async function (next) {
+//   if (this.isModified("password")) {
+//     this.password = await bcrypt.hash(this.password, 10);
+//   }
+//   next();
+// });
 
-UserSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
+// UserSchema.methods.comparePassword = function (candidatePassword) {
+//   return bcrypt.compare(candidatePassword, this.password);
+// };
 
 module.exports = mongoose.model("User", UserSchema);
