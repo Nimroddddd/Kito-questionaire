386fdaa91136219d56fd90716d283985b4fbf39a
auf <azeezumarfaruk@gmail.com>
2024-11-05 06:41:51 +0000

    added more origins

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ const initializePassport = require("./src/config/passport");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", [
+    "http://localhost:5173",
+    "https://kito-questionaire.vercel.app",
+  ]);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
@@ -38,7 +40,7 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: {
-      secure: false, // Must be false for non-HTTPS local development
+      secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
