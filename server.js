951e8851bf907123d979217cd0bde9f0f00604f8
auf <azeezumarfaruk@gmail.com>
2024-11-05 09:16:09 +0000

    updated session config

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.use(
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
-      domain: process.env.NODE_ENV === "production" ? ".vercel.app" : undefined,
+      domain:
+        process.env.NODE_ENV === "production" ? ".onrender.com" : undefined,
     },
     proxy: true,
   })
